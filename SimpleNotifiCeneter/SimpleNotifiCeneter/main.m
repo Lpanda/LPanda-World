cca8ebb30729c7593987e90d4161c55d3de9e420
Lpanda <376810857@qq.com>
2014-09-28 11:55:47 +0800

    add a project

---
+++ SimpleNotifiCeneter/SimpleNotifiCeneter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SimpleNotifiCeneter
+//
+//  Created by LPanda on 14-9-28.
+//  Copyright (c) 2014年 LPanda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
